f7f925793beaa6a318065a099a0ed12d69ff1657
Oliver Harrison <olivermharrison@gmail.com>
2019-05-07 20:50:23 +0100

    [+] Landing Page

---
+++ src/components/Landing.vue
@@ -0,0 +1,26 @@
+<template>
+    <div @click="open()">
+        <img src="../assets/envelope.png" class="animated pulse slow" :class="{bounceOut: isOpened, infinite: !isOpened}"/>
+        <p class="animated"  :class="{fadeOut: isOpened}">Tap to open.</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LandingPage extends Vue {
+
+    public isOpened = false;
+
+    public open() {
+        this.isOpened = true;
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    img {
+        width: 10rem;
+    }
+</style>
